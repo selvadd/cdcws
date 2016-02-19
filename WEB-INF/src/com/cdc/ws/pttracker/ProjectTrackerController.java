d2468cbc54c39f2a2a6c50091c8a06e098b03f5b
Selva <aselvadeenadayalan@gmail.com>
2016-02-19 16:12:33 +0530

    ProjectTrackerController commit after conversion of soap to rest ws

--- WEB-INF/src/com/cdc/ws/pttracker/ProjectTrackerController.java
+++ WEB-INF/src/com/cdc/ws/pttracker/ProjectTrackerController.java
@@ -1,14 +1,19 @@
 package com.cdc.ws.pttracker;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +33,175 @@ import com.google.gson.Gson;
 @RequestMapping(value="/services")
 public class ProjectTrackerController {
 
+	private Logger log = Logger.getLogger(ProjectTrackerController.class.getName());
+
+	//PROJECT TRACKER FUNCTIONS
+	public boolean projectTracker(int typeId, String loginId, String folderName,
+			String newFolderName,
+			String cdcId, String notes, Connection con) throws
+			LoginException {
+
+
+		ResultSet rs = null;
+		CallableStatement cstmt = null;
+		boolean insertflag = false;
+
+		try {
+
+			log("TypeID:"+typeId+"\n: LoginID: "+loginId+"\n:folderName: "+folderName+"\n: newFolderName: "+newFolderName
+					+"\n: cdcId: "+cdcId+"\n: notes: "+notes);
+
+			cstmt = con.prepareCall("{call SSP_PROJECT_TRACKER(?,?,?,?,?,?)}");
+			//TYPE ID BASED ON WHICH EDIT/INSERT/DELE FOR PT
+			cstmt.setInt(1, typeId);
+
+			//LOGIN ID
+			cstmt.setString(2, loginId);
+
+			//FOLDER NAME
+			if (folderName != null) {
+				cstmt.setString(3, folderName);
+			}
+			else {
+				cstmt.setString(3, null);
+			}
+
+			//NEW FOLDER NAME
+
+			if (newFolderName != null) {    	   
+				cstmt.setString(4, newFolderName);
+			}
+			else {
+				cstmt.setString(4, null);
+			}
+			//CDCID
+			cstmt.setString(5, cdcId);
+			//NOTES
+			if (notes != null) {
+				cstmt.setString(6, notes);
+			}
+			else {
+				cstmt.setString(6, null);
+			}
+
+
+
+			cstmt.execute();
+
+
+			insertflag=true;
+
+
+			cstmt.close();
+		}
+		catch (SQLException se) {
+			se.printStackTrace();
+
+			insertflag=false;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+
+
+		}
+		finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+
+
+			}
+			catch (SQLException se) {
+				log.error("!exception3.2!SQL error in ProjectTracker" + se);
+
+			}
+			finally {
+				try {
+					if (cstmt != null) {
+						cstmt.close();
+					}
+				}
+				catch (SQLException se) {
+					log.error("!exception3.3!SQL error in ProjectTracker" +
+							se);
+
+				}
+			}
+		}
+		return insertflag;
+	} // PROJECT TRACKER FUNCTIONS
+
+	/*
+	   type_id 1 - is for folder creation
+	   type_id 2 - is for editing/renaming folder
+	   type_id 3 - is for deleting folder and its projects
+	   type_id 4 - is for saving projects
+	   LoginId is mandatory
+	 */
+	@RequestMapping(value = "/projectTrackerModule")
+	public String projectTrackerModule(@RequestParam("sessionId") int sessionId,
+			@RequestParam("securityKey") String securityKey, @RequestParam("typeId")int typeId, @RequestParam("folderName")String folderName,
+			@RequestParam("newFolderName") String newFolderName,
+			@RequestParam("cdcId") String cdcId, @RequestParam("notes") String notes) {
+
+		Map<String,Object> map = null; 
+		Gson gson = null;
+
+		LeadManagerSessionData lmData = null;
+		boolean insertflag=false;
+		Connection con = null;
+
+		try {
+
+			gson = new Gson();
+			map = new HashMap<String,Object>();
+			boolean validSessionId = false;
+
+
+			if (con == null)	con = JDBCUtil.getDBConnectionFromDatabase();
+
+			validSessionId = LoginUtil.checkValidSession(sessionId, securityKey, con);
+
+
+			if (validSessionId == true) {
+
+				lmData = LoginUtil.getLeadManagerSessionDetails(sessionId, con);		  
+				insertflag = projectTracker(typeId, lmData.getLogin(), folderName,
+						newFolderName,
+						cdcId, notes, con);
+
+
+
+				map.put("Status", "Sucess");
+				map.put("Message",
+						String.valueOf(insertflag));
+
+			}
+			else {
+				map.put("iTotalRecords", "0");
+				map.put("Status", "Failure");
+				map.put("Message", "Invalid Session or Session Expired");
+
+			}
+
+		}
+		catch (Exception ex) {
+
+			log.error(
+					"Exception in projectTrackerModule() for projectTrackerModule " +
+							ex.getMessage());
+		}
+		finally{
+			JDBCUtil.releaseDBConnection(con);
+		}
+
+
+
+		return gson.toJson(map);
+
+	}
+
 	/**
 	 * [web service] for Geting the User's PT Folders List 
 	 * @param sessionId
@@ -35,11 +209,13 @@ public class ProjectTrackerController {
 	 * @return
 	 */
 	@RequestMapping(value="/getPTFoldersList")
-	public String wsgetProjectTrackerUserFolders(@RequestParam("sessionId") int sessionId,@RequestParam("securityKey") String securityKey){
+	public String wsgetProjectTrackerUserFolders(@RequestParam("sessionId") int sessionId, @RequestParam("securityKey") String securityKey){
+
 		Map<String,Object> map = null; 
 		Gson gson = null;
 		Connection con = null; 
 		LeadManagerSessionData lmData = null;
+
 		try {
 
 			gson = new Gson();
@@ -53,7 +229,7 @@ public class ProjectTrackerController {
 			boolean validSessionId = false;
 			validSessionId = LoginUtil.checkValidSession(sessionId, securityKey, con);
 
-			//System.out.println("getProjectTrackerProjectDetails"+"typeId"+typeId+"lmData.getLogin()"+lmData.getLogin()+cdcId);
+			//log.info("getProjectTrackerProjectDetails"+"typeId"+typeId+"lmData.getLogin()"+lmData.getLogin()+cdcId);
 			if (validSessionId == true) {
 
 				map.put("iTotalRecords", "1");
@@ -72,7 +248,7 @@ public class ProjectTrackerController {
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
-			System.out.println(
+			log.error(
 					"Exception in projectTrackerModule() for projectTrackerModule " +
 							ex.getMessage());
 		}
@@ -89,7 +265,6 @@ public class ProjectTrackerController {
 	 * @return
 	 */
 	private ArrayList getUserFolders(String loginId, Connection con){
-
 
 		ResultSet rs = null;
 		CallableStatement cstmt = null;
@@ -118,10 +293,10 @@ public class ProjectTrackerController {
 			cstmt.close();
 		}
 		catch (SQLException se) {
-			System.out.println("SQLException occurred in webservice while getting ProjectTracker folder Details" + se);
+			log.error("SQLException occurred in webservice while getting ProjectTracker folder Details" + se);
 		}
 		catch (Exception e) {
-			System.out.println("Exception occurred in webservice while getting ProjectTracker folder Details" + e);
+			log.error("Exception occurred in webservice while getting ProjectTracker folder Details" + e);
 
 		}
 		finally {
@@ -130,10 +305,9 @@ public class ProjectTrackerController {
 					rs.close();
 				}
 
-
 			}
 			catch (SQLException se) {
-				System.out.println("SQL error in webservice while getting ProjectTracker folder Details" + se);
+				log.error("SQL error in webservice while getting ProjectTracker folder Details" + se);
 
 			}
 		}
@@ -151,12 +325,12 @@ public class ProjectTrackerController {
 			@RequestParam("securityKey") String securityKey,@RequestParam("typeId") int typeId,@RequestParam("folderName") String folderName,
 			@RequestParam("newFolderName") String newFolderName,
 			@RequestParam("cdcId")String cdcId,@RequestParam("notes") String notes) {
+
 		Map<String,Object> map = null; 
 		Gson gson = null;
-
-
 		LeadManagerSessionData lmData = null;
 		Connection con = null;
+
 		try {
 
 			gson = new Gson();
@@ -167,7 +341,7 @@ public class ProjectTrackerController {
 
 			validSessionId = LoginUtil.checkValidSession(sessionId, securityKey, con);
 
-			//System.out.println("getProjectTrackerProjectDetails"+"typeId"+typeId+"lmData.getLogin()"+lmData.getLogin()+cdcId);
+			//log.info("getProjectTrackerProjectDetails"+"typeId"+typeId+"lmData.getLogin()"+lmData.getLogin()+cdcId);
 			if (validSessionId == true) {
 
 				lmData = LoginUtil.getLeadManagerSessionDetails(sessionId, con);
@@ -204,7 +378,7 @@ public class ProjectTrackerController {
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
-			System.out.println(
+			log.error(
 					"Exception in projectTrackerModule() for projectTrackerModule " +
 							ex.getMessage());
 		}
@@ -226,10 +400,7 @@ public class ProjectTrackerController {
 		CallableStatement cstmt = null;
 		ArrayList jobDetails=new ArrayList();
 
-
 		try {
-
-
 
 			cstmt = con.prepareCall("{call SSP_PROJECT_TRACKER(?,?,?,?,?)}");
 
@@ -299,7 +470,7 @@ public class ProjectTrackerController {
 
 			}
 			catch (SQLException se) {
-				System.out.println("!exception3.2!SQL error in ProjectTracker Details" + se);
+				log.error("!exception3.2!SQL error in ProjectTracker Details" + se);
 
 			}
 			finally {
@@ -309,7 +480,7 @@ public class ProjectTrackerController {
 					}
 				}
 				catch (SQLException se) {
-					System.out.println("!exception3.3!SQL error in ProjectTracker Details" +
+					log.error("!exception3.3!SQL error in ProjectTracker Details" +
 							se);
 
 				}
@@ -318,4 +489,104 @@ public class ProjectTrackerController {
 		return jobDetails;
 	}
 	// PROJECT TRACKER JOB DETAILS .
+
+	@RequestMapping(value = "/checkProjectSavedPT")
+	public String checkProjectSavedPT(@RequestParam("sessionId") int sessionId,
+			@RequestParam("securityKey") String securityKey, @RequestParam("cdcId") String cdcId) {
+
+		Map<String,Object> map = null; 
+		Gson gson = null;
+		Connection con = null;
+		LeadManagerSessionData lmData = null;
+
+		try {
+
+			gson = new Gson();
+			map = new HashMap<String,Object>();
+			boolean validSessionId = false;
+			boolean projectSaved=false;
+
+			if (con == null)	con = JDBCUtil.getDBConnectionFromDatabase();
+
+			validSessionId = LoginUtil.checkValidSession(sessionId, securityKey, con);
+
+			//log.info(validSessionId);
+			if (validSessionId == true) {
+
+				lmData = LoginUtil.getLeadManagerSessionDetails(sessionId, con);
+
+
+				projectSaved=LoginUtil.checkProjectTrackerSavedStatus(lmData.getLogin(),cdcId, con);
+
+				map.put("Status", String.valueOf(projectSaved));
+
+
+
+			}
+			else {
+				map.put("iTotalRecords", "0");
+				map.put("Status", "Failure");
+				map.put("Message", "Invalid Session or Session Expired");
+
+			}
+
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+			log.error(
+					"Exception in projectTrackerModule() for projectTrackerModule " +
+							ex.getMessage());
+		}
+		finally{
+			JDBCUtil.releaseDBConnection(con);
+		}
+
+		return gson.toJson(map);
+
+	}
+
+	/**
+	 * Init log writer
+	 */
+	public BufferedWriter logInit(){
+		String date = new SimpleDateFormat("MMddyy").format(new java.util.Date());
+		File f = new File("wslogs/webservice_"+date+".log");
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		try {
+			if(!f.exists())		
+				f.createNewFile();
+
+			fw = new FileWriter(f,true);
+			bw = new BufferedWriter(fw);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return bw;
+	}
+
+	/**
+	 * write log
+	 * @param info
+	 */
+	public void log(String info){
+
+		BufferedWriter bw = null;
+		try {
+			bw = logInit();
+			String logtime = new SimpleDateFormat("d MMM yyyy H:m:s,S").format(new java.util.Date());;
+			bw.write(logtime+"["+this.getClass().getName()+"] -"+info+"\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (bw != null) {
+				try {
+					bw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}	   
+	}
+
 }
